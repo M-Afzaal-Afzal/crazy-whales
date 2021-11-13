a2f14a806e20648e3e22d400ef2abb871ba9c45d
M Afzaal Afzal <afzaal1721@gmail.com>
2021-11-13 10:45:19 +0500

    ssr bug fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
+import config from 'react-reveal/globals';
+
+config({ ssrFadeout: true });
 
 function MyApp({ Component, pageProps }) {
+
   return <Component {...pageProps} />
 }
 
